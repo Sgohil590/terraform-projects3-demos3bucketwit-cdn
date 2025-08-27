12233d5562d21e81da04cb8e2c32445b7dbaa6f0
Sgohil590 <gohilsahil513@gmail.com>
2025-08-27 18:43:22 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,79 +1,116 @@
 
-
 provider "aws" {
   region = "us-east-1"
-
 }
 
+#######################
+# Route53 Hosted Zone #
+#######################
+resource "aws_route53_zone" "sahil_zone" {
+  name = "sahil.virals.studio"
+}
 
-#s3 Bucket
+##########################
+# S3 Bucket (Static Site)#
+##########################
 resource "aws_s3_bucket" "website" {
-  bucket        = var.bucket_name
+  bucket        = "sahil-virals-studio-website"
   force_destroy = true
 }
 
-#Enable static website hosting
 resource "aws_s3_bucket_website_configuration" "website" {
-  bucket  = aws_s3_bucket.website.id
+  bucket = aws_s3_bucket.website.id
 
-
- index_document {
-  suffix = "index.html"
-
+  index_document {
+    suffix = "index.html"
+  }
 }
-}
-
-# upload index.html
 
 resource "aws_s3_object" "index" {
   bucket       = aws_s3_bucket.website.id
   key          = "index.html"
   source       = "${path.module}/index.html"
   content_type = "text/html"
-
 }
-#public read policy 
 
 resource "aws_s3_bucket_policy" "public" {
-  bucket  = aws_s3_bucket.website.id
-  policy  = jsonencode ( {
-    Version  = "2012-10-17"
+  bucket = aws_s3_bucket.website.id
+  policy = jsonencode({
+    Version = "2012-10-17",
     Statement = [
-    { 
-     Effect    =  "Allow"
-     Principal = "*", 
-     Action    = "${aws_s3_bucket.website.arn}/*"
-     Resource  = "${aws_s3_bucket.website.arn}/*"
-   }
-]
-})
+      {
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.website.arn}/*"
+      }
+    ]
+  })
 }
 
-#cloudfront distribution 
+##########################
+# ACM Certificate (SSL)  #
+##########################
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "sahil.virals.studio"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Create DNS validation records in Route53
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  zone_id = aws_route53_zone.sahil_zone.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
+
+#############################
+# CloudFront Distribution   #
+#############################
 resource "aws_cloudfront_distribution" "cdn" {
   origin {
-   domain_name  = aws_s3_bucket_website_configuration.website.website_endpoint
-   origin_id    = "s3-origin"
+    domain_name = aws_s3_bucket_website_configuration.website.website_endpoint
+    origin_id   = "s3-origin"
 
-  custom_origin_config {
-     http_port                = 80 
-     https_port               = 443
-     origin_protocol_policy   = "http-only"
-     origin_ssl_protocols     = ["TLS1.2"]
-}
-}
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
 
-enabled              = true
-default_root_object  = "index.html"
+  enabled             = true
+  default_root_object = "index.html"
 
-default_cache_behavior {
+  default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = "s3-origin"
 
     forwarded_values {
       query_string = false
-      cookies { forward = "none" }
+      cookies {
+        forward = "none"
+      }
     }
 
     viewer_protocol_policy = "redirect-to-https"
@@ -81,12 +118,32 @@ default_cache_behavior {
 
   restrictions {
     geo_restriction {
-      restriction_type  = "none"
-}
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate.cert.arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
+
+  depends_on = [aws_acm_certificate_validation.cert_validation]
 }
 
-viewer_certificate {
- cloudfront_default_certificate = true
+##################################
+# Route53 Alias to CloudFront    #
+##################################
+resource "aws_route53_record" "alias" {
+  zone_id = aws_route53_zone.sahil_zone.zone_id
+  name    = "sahil.virals.studio"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.cdn.domain_name
+    zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
+    evaluate_target_health = false
+  }
 }
-}
+
 
